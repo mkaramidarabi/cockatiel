7ce60cafa12e42f150dcacd269043b202b6c5716
Mahdi <mkaramidarabi@gmail.com>
2022-08-01 20:30:07 +0430

    time and state added to logger function

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -3,22 +3,27 @@ import chalk from 'chalk'
 console.old_log = console.log
 
 const log = function (...messages: string[]) {
+  messages.unshift('info   :')
   console.old_log(chalk.blue(_text(messages)))
 }
 
 const start = function (...messages: string[]) {
+  messages.unshift('start  :')
   console.old_log(chalk.bgGreen.bold.black(_text(messages)))
 }
 
 const success = function (...messages: string[]) {
+  messages.unshift('success:')
   console.old_log(chalk.green(_text(messages)))
 }
 
 const error = function (...messages: string[]) {
+  messages.unshift('error  :')
   console.old_log(chalk.red(_text(messages)))
 }
 
 const warn = function (...messages: string[]) {
+  messages.unshift('warn   :')
   console.old_log(chalk.yellow(_text(messages)))
 }
 
@@ -31,9 +36,9 @@ const defineLogger = () => {
 }
 
 const _text = (messages: string[]): string => {
-  let text = ''
+  let text = new Date().toISOString()
   for (const message of messages) {
-    text += message + ' '
+    text += ' ' + message
   }
   return text
 }

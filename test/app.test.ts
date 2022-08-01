f41515cc53d6cf6274b9a2d8ce46bc264796d618
Mahdi <mkaramidarabi@gmail.com>
2022-08-01 18:11:48 +0430

    initial structure for testing created with jest

--- test/app.test.ts
+++ test/app.test.ts
@@ -4,7 +4,7 @@ import request from "supertest"
 import app from './../src/app'
 
 describe("app should run successfully", () => {
-  it("should return 15 for add(10,5)", async () => {
+  it("Application healthCheck", async () => {
     const res = await request(app).get("/")
     expect(res.body.success).toBe(true)
   })

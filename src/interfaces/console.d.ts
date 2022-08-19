6a19301e873f6b3c8f2cd2dfe1d14a639e551e1e
Mahdi <mkaramidarabi@gmail.com>
2022-08-19 21:44:40 +0430

    unknown type replaced with any type

--- src/interfaces/console.d.ts
+++ src/interfaces/console.d.ts
@@ -1,5 +1,5 @@
 interface Console {
-  old_log: (payload: any) => void
-  success: (payload: any) => void
-  start: (payload: any) => void
+  old_log: (payload: unknown) => void
+  success: (payload: unknown) => void
+  start: (payload: unknown) => void
 }
